f661465b0167c34a872500a5afa3693faced2254
szzzing <xxszzzing@gmail.com>
2023-08-04 23:21:53 +0900

    fix: 토큰 만료시 삭제

--- front/src/axios/index.js
+++ front/src/axios/index.js
@@ -3,10 +3,25 @@ import { store } from '../store/index'
 
 // axios 전역 설정
 axios.defaults.baseURL = "http://localhost:8080";
+
 // 요청을 보낼 때 상태에 저장된 token을 함께 전송
-axios.interceptors.request.use((config)=>{
-    config.headers.Authorization = store.state.auth.token;
-    return config;
-});
+axios.interceptors.request.use(
+    (config)=>{
+        config.headers.Authorization = store.state.auth.token;
+        return config;
+    },
+);
+
+// 토큰이 만료되었을 시 전역 상태에서 삭제
+axios.interceptors.response.use(
+    (config) => {
+        return config
+    },
+    (error) => {
+        console.log(error);
+        store.commit("auth/setToken", "");
+        store.commit("auth/setUser", null);
+    }
+)
 
 export default axios;
