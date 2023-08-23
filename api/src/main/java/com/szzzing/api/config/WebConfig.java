5c0687c50bb7317986f807679dc97762c4accda0
szzzing <xxszzzing@gmail.com>
2023-08-23 10:53:35 +0900

    fix: 정적 리소스 위치 변경

--- api/src/main/java/com/szzzing/api/config/WebConfig.java
+++ api/src/main/java/com/szzzing/api/config/WebConfig.java
@@ -6,7 +6,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-    private final String profilePath = "/Users/szzzing/Mine/project/Reminiscene/upload/profile/";
+//    private final String profilePath = "/Users/szzzing/Mine/project/Reminiscene/upload/profile/";
+    private final String profilePath = "../../Reminiscene/upload/profile/";
 
     // 프로필 이미지 경로를 정적 자원으로 인식해 접근하도록 설정
     @Override
